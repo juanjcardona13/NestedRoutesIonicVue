f61da4d1c1218a76e3719dd7384d7bd70bd594a2
juan.guzman <jose.guzman@xpertgroup.co>
2021-12-22 09:45:09 -0500

    Init project

---
+++ src/views/GenericLayoutPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <ion-page id="WizardLayout">
+    <ion-header class="ion-no-border">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button color="dark" default-href="/tabs/tab1" @click="() => $router.push('/tabs/tab1')" text="" :icon="arrowBackOutline">
+          </ion-back-button>
+        </ion-buttons>
+        <ion-title>{{tituloHeader}}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ion-router-outlet />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
+    IonContent,
+    IonRouterOutlet,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+import { arrowBackOutline } from "ionicons/icons";
+
+export default defineComponent({
+  name: "WizardLayout",
+  setup() {
+    return {
+      arrowBackOutline,
+    };
+  },
+  data() {
+    return {
+      tituloHeader: "CREAR"
+    }
+  },
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
+    IonContent,
+    IonRouterOutlet,
+  },
+});
+</script>
---
+++ src/views/crearActualizarPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <ion-page>
+    <ion-text color="primary">
+        <h1>CREAR ACTUALIZAR PAGE</h1>
+    </ion-text>
+  </ion-page>
+</template>
+
+<script >
+import { IonPage, IonText } from '@ionic/vue';
+
+export default  {
+  components: { IonText, IonPage }
+}
+</script>
